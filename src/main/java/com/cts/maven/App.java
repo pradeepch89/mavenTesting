59644d5bc93d053aa62adec8bbe007830a51959f
Pradeep <pradeep.chellappa@gmail.com>
2018-09-24 23:54:50 -0500

    my first commit

---
+++ src/main/java/com/cts/maven/App.java
@@ -0,0 +1,13 @@
+package com.cts.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
